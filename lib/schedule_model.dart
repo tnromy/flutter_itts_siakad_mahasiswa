ad51c129dfb245e92d1999e2481c2162ae1ed00f
tnromy <tuanromy@gmail.com>
2023-11-24 20:45:32 +0700

    menambahkan list jadwal perkuliahan beserta foto dosen, nama mata kuliah, nama dosen dan jam perkuliahan

---
+++ lib/schedule_model.dart
@@ -0,0 +1,48 @@
+class ScheduleModel {
+  List<Map<String, dynamic>> schedules() {
+    return [
+      {
+        "name": "Pemrograman .NET",
+        "l_ava": "1.jpg",
+        "l_full_name": "Taufik Iqbal Ramdhani, S.Kom., M.Sc.",
+        "schedule_time": "17.00"
+      },
+      {
+        "name": "Pemrosesan Bahasa Alami",
+        "l_ava": "2.jpg",
+        "l_full_name": "Muhamad Yusuf, S.Kom., M.Kom.",
+        "schedule_time": "19.00"
+      },
+      {
+        "name": "Sistem Manajemen Basis Data",
+        "l_ava": "3.jpg",
+        "l_full_name": "Anas Nasrulloh, S.Kom., M.Kom.",
+        "schedule_time": "17.00"
+      },
+      {
+        "name": "Visualisasi 3D",
+        "l_ava": "4.jpg",
+        "l_full_name": "Muhammad Akbar Maulana, S.Kom., M.Kom.",
+        "schedule_time": "17.00"
+      },
+      {
+        "name": "Etika Profesi",
+        "l_ava": "5.jpg",
+        "l_full_name": "Tubagus Toifur, S.Kom., M.Kom.",
+        "schedule_time": "19.00"
+      },
+      {
+        "name": "Keamanan Informasi & Jaringan",
+        "l_ava": "6.jpg",
+        "l_full_name": "Andi Kriswantono, S.Kom., M.Kom.",
+        "schedule_time": "17.00"
+      },
+      {
+        "name": "Pemrograman Aplikasi Mobile",
+        "l_ava": "7.jpg",
+        "l_full_name": "Taufik Iqbal Ramdhani, S.Kom., M.Sc.",
+        "schedule_time": "19.00"
+      },
+    ];
+  }
+}
--- lib/schedule_view.dart
+++ lib/schedule_view.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import './schedule_model.dart';
 
 class ScheduleView extends StatelessWidget {
-  const ScheduleView({super.key});
+  final scheduleM = ScheduleModel();
 
   @override
   Widget build(BuildContext context) {
-    return Text("ini schedule");
+    return ListView(
+      children: scheduleM.schedules().asMap().entries.map((e) {
+        return Container(
+          margin: EdgeInsets.all(2),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            elevation: 1,
+            color: Colors.white,
+            child: Container(
+              margin: EdgeInsets.all(5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/" + e.value["l_ava"]),
+                  ),
+                  Container(
+                    width: 300,
+                    margin: EdgeInsets.symmetric(horizontal: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(e.value["name"]),
+                        Text(e.value["l_full_name"]),
+                      ],
+                    ),
+                  ),
+                  Text(e.value["schedule_time"]),
+                ],
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    );
   }
 }
