57b66cffdaef4ea0efe9b516e0d99c5cd023b9a6
tnromy <tuanromy@gmail.com>
2023-11-24 21:17:13 +0700

    membuat layout tampilan foto circular di home

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -5,6 +5,87 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("ini home");
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // row 1
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: Row(
+              children: [
+                // row 1 image 1
+                Container(
+                  width: (MediaQuery.of(context).size.width) / 2,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/1.jpg"),
+                      ),
+                    ),
+                  ),
+                ),
+                
+                // row 1 image 2
+                Container(
+                  width: (MediaQuery.of(context).size.width) / 2,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/1.jpg"),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          // row 2
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: Row(
+              children: [
+                // row 2 image 1
+                Container(
+                  width: (MediaQuery.of(context).size.width) / 2,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/1.jpg"),
+                      ),
+                    ),
+                  ),
+                ),
+                
+                // row 2 image 2
+                Container(
+                  width: (MediaQuery.of(context).size.width) / 2,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/1.jpg"),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
